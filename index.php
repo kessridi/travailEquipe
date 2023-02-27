e2a6209da8bf724f4dfffe20e3b94d71b479aa22
A-Olichi <aris.olichi7@gmail.com>
2023-02-27 15:19:54 +0100

    second first

--- index.php
+++ index.php
@@ -13,20 +13,9 @@
 
 
 <body>
-    <h1>Contactez-nous</h1>
-     <?php include 'contact.php'; ?>
-    <form method="post" action="" style="display: flex; flex-direction: column; align-items: center; justify-content: center; margin-top: 50px; font-size: 18px;">
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom" placeholder="Entrez votre nom" required>
 
-        <label for="email">Email :</label>
-        <input type="email" id="email" name="email" placeholder="Entrez votre email" required>
-
-        <label for="message">Message :</label>
-        <textarea id="message" name="message" placeholder="Entrez votre message" required></textarea>
-
-        <button type="submit" style="padding: 10px; border-radius: 5px; border: none; background-color: #007bff; color: #fff; font-size: 18px; cursor: pointer;">Envoyer</button>
-    </form>
+    <?php include 'contact.php'; ?>
+    
 </body>
 
 
